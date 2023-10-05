1cc553cb20140c4c4f5a17e2b354aea2acb6c78b
devops <ngunq@yes4all.com>
2023-10-05 21:47:39 +0700

    devops-update

--- requirement_1/app.js
+++ requirement_1/app.js
@@ -8,27 +8,59 @@ const app = express();
 
 // Load filesByHash from a JSON file if it exists
 let filesByHash = {};
+let filesMapping = {}
 
+const UPLOAD_DIR = "uploads"
 const FILES_BY_HASH_FILE = path.join(__dirname, 'filesByHash.json');
+const FILES_MAPPING_FILE = path.join(__dirname, 'filesMapping.json');
 
-const saveFilesByHash = () => {
-    fs.writeFileSync(FILES_BY_HASH_FILE, JSON.stringify(filesByHash), 'utf-8');
+const saveFiles = (filepath, data) => {
+    fs.writeFileSync(filepath, JSON.stringify(data), 'utf-8');
 };
 
-const loadFilesByHash = () => {
-    if (fs.existsSync(FILES_BY_HASH_FILE)) {
-        const data = fs.readFileSync(FILES_BY_HASH_FILE, 'utf-8');
-        filesByHash = JSON.parse(data);
+const loadFiles = (filepath) => {
+    if (fs.existsSync(filepath)) {
+        const data = fs.readFileSync(filepath, 'utf-8');
+        return JSON.parse(data);
     }
+    return {};
 };
 
-loadFilesByHash();
+const updateFilesMapping = (filepath) => {
+    let mappings = Object.keys(filesMapping).map(key => {
+        if (filesMapping[key] == filepath) {
+            return key;
+        }
+    });
+
+    if (mappings.length == 0) {
+        return null
+    }
+
+
+
+    if (mappings.length > 1) {
+        for (let index = 1; index < mappings.length; index++) {
+            filesMapping[mappings[index]] = path.join(UPLOAD_DIR, mappings[0])
+        }
+    }
+
+    delete filesMapping[mappings[0]]
+    fs.copyFileSync(filepath, path.join(UPLOAD_DIR, mappings[0]))
+
+    return mappings[0]
+}
+
+
+filesByHash = loadFiles(FILES_BY_HASH_FILE);
+filesMapping = loadFiles(FILES_MAPPING_FILE);
 
 // Define storage for uploaded files
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        const uploadDir = './uploads/';
+        const uploadDir = './tmp';
         fs.mkdirSync(uploadDir, { recursive: true });
+        fs.mkdirSync("./" + UPLOAD_DIR, { recursive: true });
         cb(null, uploadDir);
     },
     filename: (req, file, cb) => {
@@ -45,37 +77,79 @@ const getFileHash = (filePath) => {
     return hash.digest('hex');
 };
 
-app.use('/files', express.static(path.join(__dirname, 'uploads')));
+const moveFileFromTemporary = (filename) => {
+    let source = path.join("tmp", filename)
+    let destination = path.join(UPLOAD_DIR, filename)
+    fs.copyFileSync(source, destination)
+    fs.unlinkSync(source)
+}
 
 // Upload a new file
 app.post('/upload', upload.single('file'), (req, res) => {
     const { file } = req;
+    const fileHash = getFileHash(file.path)
+    const fileName = file.originalname;
+    const filePath = path.join(UPLOAD_DIR, fileName)
 
-    const fileHash = getFileHash(file.path);
+    const existingFileWithSameName = Object.values(filesByHash).find(file => file.name === fileName);
 
-    if (filesByHash[fileHash]) {
+    if (existingFileWithSameName) {
+        const oldFileHash = getFileHash(filePath)
+        if (filesByHash[fileHash]) { // File exist but have same content with other file
+            fs.unlinkSync(file.path);
+            fs.unlinkSync(filePath);
+            delete filesByHash[oldFileHash]
+
+            const existingFile = filesByHash[fileHash];
+            filesMapping[fileName] = existingFile.path
+            res.send('File uploaded successfully (reused content of ' + existingFile.name + ')');
+
+        } else { // File exist with new content
+            filesByHash[fileHash] = { path: filePath, name: fileName };
+            let updateFile = updateFilesMapping(filePath)
+            if (updateFile) {
+                // fs.copyFileSync(filePath, path.join(UPLOAD_DIR, updateFile))
+                filesByHash[oldFileHash] = { path: path.join(UPLOAD_DIR, updateFile), name: updateFile };
+            }
+            moveFileFromTemporary(fileName)
+            res.send('File uploaded successfully (overwite)');
+
+        }
+
+    } else if (filesByHash[fileHash]) { // New file have same content with other file
         fs.unlinkSync(file.path);
-        const existingFile = filesByHash[fileHash];
-        const symlinkPath = path.join(__dirname, 'uploads', file.originalname);
-        fs.symlinkSync(existingFile.path, symlinkPath);
-        res.send('File uploaded successfully (reused content)');
-    } else {
-        filesByHash[fileHash] = { path: file.path, name: file.originalname };
-        res.send('File uploaded successfully');
 
-        saveFilesByHash();
+        // File with the same content already exists, create a reference mapping
+        const existingFile = filesByHash[fileHash];
+        filesMapping[fileName] = existingFile.path
+        res.send('File uploaded successfully (reused content of ' + existingFile.name + ')');
+    } else {
+        // File with this content doesn't exist, store it and update hash mapping & reference mapping
+        if (filesMapping[fileName])
+            delete filesMapping[fileName];
+        filesByHash[fileHash] = { path: filePath, name: fileName };
+        moveFileFromTemporary(fileName)
+        res.send('File uploaded successfully');
     }
+
+    // Update the persisted data
+    saveFiles(FILES_BY_HASH_FILE, filesByHash);
+    saveFiles(FILES_MAPPING_FILE, filesMapping);
 });
+
 
 
 // Retrieve an uploaded file by name
 app.get('/files/:filename', (req, res) => {
     const { filename } = req.params;
     const filePath = path.join(__dirname, 'uploads', filename);
-    try {
-        const resolvedPath = fs.realpathSync(filePath);
-        res.sendFile(resolvedPath);
-    } catch (error) {
+
+    // Check if the file exists
+    if (fs.existsSync(filePath)) {
+        res.sendFile(filePath);
+    } else if (filesMapping[filename]) {
+        res.sendFile(path.resolve(filesMapping[filename]));
+    } else {
         res.status(404).send('File not found');
     }
 });
@@ -84,18 +158,30 @@ app.get('/files/:filename', (req, res) => {
 // Delete an uploaded file by name
 app.delete('/files/:filename', (req, res) => {
     const { filename } = req.params;
-    const filePath = path.join(__dirname, 'uploads', filename);
+    const filePath = path.join('uploads', filename);
 
     if (fs.existsSync(filePath)) {
         const fileHash = getFileHash(filePath);
         delete filesByHash[fileHash];
 
+        let updateFile = updateFilesMapping(filePath)
+        if (updateFile) {
+            // fs.copyFileSync(filePath, path.join(UPLOAD_DIR, updateFile))
+            filesByHash[fileHash] = { path: path.join(UPLOAD_DIR, updateFile), name: updateFile };
+        }
         fs.unlinkSync(filePath);
-        saveFilesByHash();
+
+        res.send('File deleted successfully');
+    } else if (filesMapping[filename]) {
+        // If file still reference to other file, just remove this reference
+        delete filesMapping[filename];
         res.send('File deleted successfully');
     } else {
         res.status(404).send('File not found');
     }
+
+    saveFiles(FILES_BY_HASH_FILE, filesByHash);
+    saveFiles(FILES_MAPPING_FILE, filesMapping);
 });
 
 const PORT = process.env.PORT || 3000;
